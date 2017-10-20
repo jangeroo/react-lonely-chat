95ba0f9656463de04dc2321fe7c62cb00da87106
Michael Jang <michaeljang86@gmail.com>
2017-10-20 11:11:19 -0400

    Messages stored in state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = { messages: [
+                    {name: 'john', content: 'Hello'},
+                    {name: 'bob', content: 'How are you doing'},
+                    {name: 'john', content: 'Fine, thank you'}
+                  ]}
+  }
+
   displayMessage = (message) => (
     <div>
       <span><b>{message.name}</b> </span>
@@ -11,18 +20,12 @@ class App extends Component {
   )
 
   render() {
-    
-    var messages = [
-      {name: 'john', content: 'Hello'},
-      {name: 'bob', content: 'How are you doing'},
-      {name: 'john', content: 'Fine, thank you'}
-    ]
 
     return (
       <div>
 
         <div id='message-display'>
-          {messages.map(this.displayMessage)}
+          {this.state.messages.map(this.displayMessage)}
         </div>
 
         <div id='message-input'>
