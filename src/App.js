892b025475c2be9c90aa5e259648f7f7770a7af3
Michael Jang <michaeljang86@gmail.com>
2017-10-20 11:02:28 -0400

    Static page: messages with input and button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+
+        <div id='message-display'>
+          <div className='message'>
+            <span className='name'><b>john</b> </span>
+            <span className='message-content'>Hello</span>
+          </div>
+          <div className='message'>
+            <span className='name'><b>bob</b> </span>
+            <span className='message-content'>How are you doing</span>
+          </div>
+          <div className='message'>
+            <span className='name'><b>john</b> </span>
+            <span className='message-content'>Fine, thank you</span>
+          </div>
+        </div>
+
+        <div id='message-input'>
+          <form>
+            <input/>
+            <button type='submit'>send</button>
+          </form>
+        </div>
+
       </div>
     );
   }
