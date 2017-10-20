7484b66e2cb8b43069fba9fce0210258a84fc239
Michael Jang <michaeljang86@gmail.com>
2017-10-20 11:59:15 -0400

    Submit and response
    Form now submits message and BOT responses with a canned response.

--- src/App.js
+++ src/App.js
@@ -10,18 +10,23 @@ class App extends Component {
 
   componentDidMount() {
     [
-      {name: 'john', content: 'Hello'},
-      {name: 'bob', content: 'How are you doing'},
-      {name: 'john', content: 'Fine, thank you'}
+      {name: 'michael', content: 'Hello'},
+      {name: 'BOT', content: 'How are you doing'},
+      {name: 'michael', content: 'Fine, thank you'}
     ].map(this.addMessage)
-
-    setInterval(this.addMessage, 1000, ({name: 'troll', content: 'Boo!'}))
   }
 
   addMessage = message => {
     this.setState(st => ({messages: st.messages.concat(message)}))
   }
 
+  submitFunction = event => {
+    event.preventDefault()
+    this.addMessage({name: 'michael', content: this.input.value})
+    setTimeout(this.addMessage, 1000, ({name: 'BOT', content: 'Hello, I\'m not a bot'}))
+    this.form.reset()
+  }
+ 
   displayMessage = (message) => (
     <div>
       <span><b>{message.name}</b> </span>
@@ -39,8 +44,8 @@ class App extends Component {
         </div>
 
         <div id='message-input'>
-          <form>
-            <input/>
+          <form ref={r => this.form = r} onSubmit={this.submitFunction}>
+            <input ref={r => this.input = r}/>
             <button type='submit'>send</button>
           </form>
         </div>
